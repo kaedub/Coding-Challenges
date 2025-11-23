0c823f5d16226184fe539bf9ae7358ad8229807c
Kevin Welch <kaedub16@gmail.com>
2025-11-22 23:41:27 -0800

    Rust message_parser revision

--- message_parser/solutions/rust/src/main.rs
+++ message_parser/solutions/rust/src/main.rs
@@ -1,59 +1,82 @@
 mod queue;
 
-use crate::queue::MessageType;
+use crate::queue::{MessageType, QueueEntry};
 
 #[derive(Debug)]
 enum ParseError {
-    InvalidFormat,
+    InvalidFormat(String),
 }
 
 #[derive(Debug)]
 struct Message {
     message_type: MessageType,
-    // timestamp: String,
-    // sender: String,
-    // receiver: String,
-    // content: String,
-    raw: String,
+    sender: String,
+    receiver: String,
+    content: String,
 }
 
-trait Parse {
-    fn parse(&self, raw: &str) -> Result<Message, ParseError>;
+trait ParseLine {
+    fn parse_line(&self, section: &str, text: &str) -> Result<String, ParseError>;
 }
 
 struct EmailParser;
 
-impl Parse for EmailParser {
-    fn parse(&self, raw: &str) -> Result<Message, ParseError> {
-        let msg = Message {
-            message_type: MessageType::Email,
-            raw: raw.to_string(),
-        };
-        Ok(msg)
+impl EmailParser {
+    fn parse_email_line(&self, text: &str) -> String {
+        let email = text.replace('<', "").replace('>', "");
+        email.trim().to_string()
+    }
+
+    fn parse_body_line(&self, text: &str) -> String {
+        text.trim().to_string()
+    }
+}
+
+impl ParseLine for EmailParser {
+    fn parse_line(&self, section: &str, text: &str) -> Result<String, ParseError> {
+        match section.trim() {
+            "To" => Ok(self.parse_email_line(text)),
+            "From" => Ok(self.parse_email_line(text)),
+            "Body" => Ok(self.parse_body_line(text)),
+            _ => Err(ParseError::InvalidFormat(
+                format!("Failed to parse Email line {}: {}", section, text,).to_string(),
+            )),
+        }
     }
 }
 
 struct SMSParser;
 
-impl Parse for SMSParser {
-    fn parse(&self, raw: &str) -> Result<Message, ParseError> {
-        let msg = Message {
-            message_type: MessageType::SMS,
-            raw: raw.to_string(),
-        };
-        Ok(msg)
+impl ParseLine for SMSParser {
+    /*
+    Sender: +1234567890
+    Receiver: +0987654321
+    Date: 2021-06-21 09:00:00
+    Text: Hey! Just wanted to remind you about our meeting tomorrow at 10 AM.
+     */
+    fn parse_line(&self, section: &str, text: &str) -> Result<String, ParseError> {
+        match section.trim() {
+            "Sender" => Ok(text.trim().to_string()),
+            "Receiver" => Ok(text.trim().to_string()),
+            "Text" => Ok(text.trim().to_string()),
+            _ => Err(ParseError::InvalidFormat(
+                format!("Failed to parse SMS line {}: {}", section, text,).to_string(),
+            )),
+        }
     }
 }
 
 struct SlackParser;
-
-impl Parse for SlackParser {
-    fn parse(&self, raw: &str) -> Result<Message, ParseError> {
-        let msg = Message {
-            message_type: MessageType::Slack,
-            raw: raw.to_string(),
-        };
-        Ok(msg)
+impl ParseLine for SlackParser {
+    fn parse_line(&self, section: &str, text: &str) -> Result<String, ParseError> {
+        match section.trim() {
+            "Sender" => Ok(text.trim().to_string()),
+            "Receiver" => Ok(text.trim().to_string()),
+            "Text" => Ok(text.trim().to_string()),
+            _ => Err(ParseError::InvalidFormat(
+                format!("Failed to parse Slack line {}: {}", section, text,).to_string(),
+            )),
+        }
     }
 }
 
@@ -63,41 +86,83 @@ enum Parser {
     Slack(SlackParser),
 }
 
-impl Parse for Parser {
-    fn parse(&self, raw: &str) -> Result<Message, ParseError> {
-        match self {
-            Parser::Email(p) => p.parse(raw),
-            Parser::SMS(p) => p.parse(raw),
-            Parser::Slack(p) => p.parse(raw),
+impl Parser {
+    fn parse(&self, entry: QueueEntry) -> Result<Message, ParseError> {
+        let mut content = String::new();
+        let mut sender = String::new();
+        let mut receiver = String::new();
+        for line in entry.body.lines() {
+            if let Some((section, text)) = line.split_once(": ") {
+                let section = section.trim();
+                let text = match self {
+                    Parser::Email(p) => p.parse_line(section, text),
+                    Parser::SMS(p) => p.parse_line(section, text),
+                    Parser::Slack(p) => p.parse_line(section, text),
+                };
+
+                match section {
+                    "To" | "Sender" => {
+                        if let Ok(val) = text {
+                            sender = val
+                        }
+                    }
+                    "From" | "Receiver" => {
+                        if let Ok(val) = text {
+                            receiver = val
+                        }
+                    }
+                    "Body" | "Text" => {
+                        if let Ok(val) = text {
+                            content = val
+                        }
+                    }
+                    _ => {}
+                }
+            }
+        }
+        if content.is_empty() || sender.is_empty() || receiver.is_empty() {
+            Err(ParseError::InvalidFormat(format!(
+                "Could not parse {} message: `{}`",
+                entry.message_type, entry.body
+            )))
+        } else {
+            let msg = Message {
+                content,
+                sender,
+                receiver,
+                message_type: entry.message_type,
+            };
+            Ok(msg)
         }
     }
 }
 
-fn parser_factory(msg_type: MessageType) -> Option<Parser> {
-    match msg_type {
-        MessageType::Email => Some(Parser::Email(EmailParser)),
-        MessageType::SMS => Some(Parser::SMS(SMSParser)),
-        MessageType::Slack => Some(Parser::Slack(SlackParser)),
-    }
-}
-
-fn parse_message(entry: queue::QueueEntry) -> Result<Message, ParseError>{
-    let body = entry.body;
-    let parser = parser_factory(entry.message_type);
-    if let Some(p) = parser {
-        p.parse(&body)
-    } else {    
-        Err(ParseError::InvalidFormat)
-    }
+fn parse_message(entry: QueueEntry) -> Result<Message, ParseError> {
+    let parser = match entry.message_type {
+        MessageType::Email => Parser::Email(EmailParser),
+        MessageType::SMS => Parser::SMS(SMSParser),
+        MessageType::Slack => Parser::Slack(SlackParser),
+    };
+    Ok(parser.parse(entry)?)
 }
 
 fn main() {
+    println!("Starting...");
     let queue = queue::build_queue();
     let mut messages = Vec::new();
+    let mut errors: Vec<String> = Vec::new();
     for entry in queue {
-        if let Ok(message) = parse_message(entry) {
-            messages.push(message);
+        match parse_message(entry) {
+            Ok(m) => messages.push(m),
+            Err(e) => errors.push(format!("{:#?}", e)),
         }
     }
-    println!("Parsed messages: {:?}", messages);
+    println!("Messages: {}", messages.len());
+    for m in messages {
+        println!("{:#?}", m);
+    }
+    println!("Errors: {}", errors.len());
+    for e in errors {
+        println!("{}", e);
+    }
 }
--- message_parser/solutions/rust/src/queue.rs
+++ message_parser/solutions/rust/src/queue.rs
@@ -1,8 +1,20 @@
+use std::fmt;
+
 #[derive(Debug)]
 pub enum MessageType {
     Email,
     SMS,
     Slack,
+}
+impl fmt::Display for MessageType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let s = match self {
+            MessageType::Email => "email",
+            MessageType::SMS => "text",
+            MessageType::Slack => "slack",
+        };
+        write!(f, "{}", s)
+    }
 }
 
 #[derive(Debug)]
@@ -18,7 +30,7 @@ const EMAIL: &str = "
     Subject: Meeting Reminder
     Date: Mon, 21 Jun 2021 10:00:00 -0400
 
-    Don't forget about our meeting tomorrow at 10 AM!
+    Body: Don't forget about our meeting tomorrow at 10 AM!
 ";
 const SMS: &str = "
     Sender: +1234567890
@@ -27,8 +39,9 @@ const SMS: &str = "
     Text: Hey! Just wanted to remind you about our meeting tomorrow at 10 AM.
 ";
 const SLACK: &str = "\"Mordecai Oshea\"->\"Buxbaum Bixby\"::2025-11-22T21:22:45Z::\"Hey! Just wanted to remind you about our meeting tomorrow at 10 AM.\"";
+const INVALID: &str = "This is an invalid message format.";
 
-pub fn build_queue() -> [QueueEntry; 3] {
+pub fn build_queue() -> [QueueEntry; 4] {
     [
         QueueEntry {
             message_type: MessageType::Email,
@@ -41,6 +54,10 @@ pub fn build_queue() -> [QueueEntry; 3] {
         QueueEntry {
             message_type: MessageType::Slack,
             body: SLACK.to_string(),
+        },
+        QueueEntry {
+            message_type: MessageType::Email,
+            body: INVALID.to_string(),
         },
     ]
 }
